bb6ef26a121acea119d8c20b1887f2500125ee0b
Felipe Manchester <f.p2809@gmail.com>
2023-08-01 09:28:26 -0300

    fixing accordion

--- app/components/Accordion.js
+++ app/components/Accordion.js
@@ -4,100 +4,65 @@ import { Accordion } from "flowbite-react";
 
 export default function DefaultAccordion() {
   return (
-    <Accordion>
-      <Accordion.Panel>
-        <Accordion.Title>What is Flowbite?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            <p>
-              Flowbite is an open-source library of interactive components built
-              on top of Tailwind CSS including buttons, dropdowns, modals,
-              navbars, and more.
-            </p>
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            <p>Check out this guide to learn how to </p>
-            <a
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-              href="https://flowbite.com/docs/getting-started/introduction/"
-            >
-              <p>get started</p>
-            </a>
-            <p>
-              and start developing websites even faster with components on top
-              of Tailwind CSS.
-            </p>
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Is there a Figma file available?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            <p>
-              Flowbite is first conceptualized and designed using the Figma
-              software so everything you see in the library has a design
-              equivalent in our Figma file.
-            </p>
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            <p>Check out the</p>
-            <a
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-              href="https://flowbite.com/figma/"
-            >
-              <p>Figma design system</p>
-            </a>
-            <p>
-              based on the utility classes from Tailwind CSS and components from
-              Flowbite.
-            </p>
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>
-          What are the differences between Flowbite and Tailwind UI?
+    <Accordion className="bg-white">
+      <Accordion.Panel className="bg-white">
+        <Accordion.Title className="bg-white hover:bg-gray-50 transition duration-300">
+          Galaxy
         </Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
+        <Accordion.Content className="bg-white">
+          <div className="flex gap-7">
+            <img src="https://www.typewolf.com/assets/img/sweatshirt.jpg" />
             <p>
-              The main difference is that the core components from Flowbite are
-              open source under the MIT license, whereas Tailwind UI is a paid
-              product. Another difference is that Flowbite relies on smaller and
-              standalone components, whereas Tailwind UI offers sections of
-              pages.
+              O Galaxy é um miniblog feito com React no Front-End e Firebase no
+              Back-end. Nele você pode criar, editar e apagar suas postagens. O
+              design foi inspirado no nosso universo.
             </p>
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          </div>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel className="bg-white">
+        <Accordion.Title className="bg-white hover:bg-gray-50 transition duration-300">
+          Mysterious Word
+        </Accordion.Title>
+        <Accordion.Content className="bg-white">
+          <div className="flex gap-7">
+            <img src="https://www.typewolf.com/assets/img/sweatshirt.jpg" />
             <p>
-              However, we actually recommend using both Flowbite, Flowbite Pro,
-              and even Tailwind UI as there is no technical reason stopping you
-              from using the best of two worlds.
+              O Galaxy é um miniblog feito com React no Front-End e Firebase no
+              Back-end. Nele você pode criar, editar e apagar suas postagens. O
+              design foi inspirado no nosso universo.
             </p>
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Learn more about these technologies:
-          </p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-            <li>
-              <a
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-                href="https://flowbite.com/pro/"
-              >
-                <p>Flowbite Pro</p>
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-                href="https://tailwindui.com/"
-                rel="nofollow"
-              >
-                <p>Tailwind UI</p>
-              </a>
-            </li>
-          </ul>
+          </div>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel className="bg-white">
+        <Accordion.Title className="bg-white hover:bg-gray-50 transition duration-300">
+          To-Do List
+        </Accordion.Title>
+        <Accordion.Content className="bg-white">
+          <div className="flex gap-7">
+            <img src="https://www.typewolf.com/assets/img/sweatshirt.jpg" />
+            <p>
+              O Galaxy é um miniblog feito com React no Front-End e Firebase no
+              Back-end. Nele você pode criar, editar e apagar suas postagens. O
+              design foi inspirado no nosso universo.
+            </p>
+          </div>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel className="bg-white">
+        <Accordion.Title className="bg-white hover:bg-gray-50 transition duration-300">
+          DoctorCare
+        </Accordion.Title>
+        <Accordion.Content className="bg-white">
+          <div className="flex gap-7">
+            <img src="https://www.typewolf.com/assets/img/sweatshirt.jpg" />
+            <p>
+              O Galaxy é um miniblog feito com React no Front-End e Firebase no
+              Back-end. Nele você pode criar, editar e apagar suas postagens. O
+              design foi inspirado no nosso universo.
+            </p>
+          </div>
         </Accordion.Content>
       </Accordion.Panel>
     </Accordion>
--- app/components/Project.js
+++ app/components/Project.js
@@ -30,24 +30,8 @@ const Project = () => {
           Para aplicativos web como o miniblog Galaxy eu utilizo o React. Design
           e código são minhas paixões ❤️
         </p>
-
-        <div className="bg-white rounded-md">
-          <h3>Galaxy</h3>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-          </ul>
-          <img src="https://www.typewolf.com/assets/img/sweatshirt.jpg" />
-          <p>
-            O Galaxy é um miniblog feito com React no Front-End e Firebase no
-            Back-end. Nele você pode criar, editar e apagar suas postagens. O
-            design foi inspirado no nosso universo.
-          </p>
-        </div>
+        <DefaultAccordion />
       </div>
-      <DefaultAccordion />
     </section>
   );
 };
